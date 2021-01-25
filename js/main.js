c5f24920e2619ee7ac902ae27954fe754cd7173d
nhanhoang863 <49527414+nhanhoang863@users.noreply.github.com>
2021-01-25 18:27:27 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function () {
+    'use strict'
+
+    $("[data-trigger]").on("click", function () {
+        var trigger_id = $(this).attr('data-trigger');
+        $(trigger_id).toggleClass("show");
+        $('body').toggleClass("offcanvas-active");
+    });
+
+    $(document).on('keydown', function (event) {
+        if (event.keyCode === 27) {
+            $(".navbar-collapse").removeClass("show");
+            $("body").removeClass("overlay-active");
+        }
+    });
+
+    $(".btn-close").click(function (e) {
+        $(".navbar-collapse").removeClass("show");
+        $("body").removeClass("offcanvas-active");
+    });
+
+
+})
